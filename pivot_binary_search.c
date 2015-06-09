89126fb3e851871b9338667b8a6a87edf605a635
vnrk <koppu.venkata@gmail.com>
2015-06-09 23:06:42 +0530

    added finding pivot in iteravtive bs

--- pivot_binary_search.c
+++ pivot_binary_search.c
@@ -97,3 +97,18 @@ int rotated_binary_search(int A[], int N, int key) {
   }
   return -1;
 }
+
+//linear way to find the pivot 
+int FindSortedArrayRotation(int A[], int N) {
+  int L = 0;
+  int R = N - 1;
+  
+  while (A[L] > A[R]) {
+    int M = L + (R - L) / 2;
+    if (A[M] > A[R])
+      L = M + 1;
+    else
+      R = M;
+  }
+  return L;
+}
