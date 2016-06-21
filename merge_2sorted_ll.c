ca14759caedb45ad079d2cbb506d6184a1ffc4dd
vnrk <koppu.venkata@gmail.com>
2016-06-21 14:01:56 +0530

    updated with iterative merge of linked list

--- merge_2sorted_ll.c
+++ merge_2sorted_ll.c
@@ -6,27 +6,52 @@
 /* Link list node */
 struct node
 {
-    int data;
-    struct node* next;
+   int data;
+   struct node* next;
 };
 
 void push(struct node** head_ref, int new_data)
 {
-    struct node* new_node =
-        (struct node*) malloc(sizeof(struct node));
-    new_node->data  = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref)    = new_node;
+   struct node* new_node =
+       (struct node*) malloc(sizeof(struct node));
+   new_node->data  = new_data;
+   new_node->next = (*head_ref);
+   (*head_ref)    = new_node;
 }
 
 /* Function to print nodes in a given linked list */
 void printList(struct node *node)
 {
-    while (node!=NULL)
-    {
-        printf("%d ", node->data);
-        node = node->next;
-    }
+   while (node!=NULL)
+   {
+       printf("%d ", node->data);
+       node = node->next;
+   }
+}
+
+struct node* mergeList(struct node* a, struct node* b){
+	struct node dummy;
+	struct node* tail = &dummy;
+	while(true){
+		if(a == NULL){
+			tail->next = b;
+			break;
+		}
+			if(b == NULL){
+			tail->next = a;
+			break;
+		}
+		if(a->info <= b->info){
+			tail->next = a;
+			tail = tail->next;
+			a = a->next;
+		}else{
+			tail->next = b;
+			tail = tail->next;
+			b = b->next;
+		}
+	}
+	return dummy.next;
 }
 
 struct node * SortedMerge(struct node *a, struct node *b)
@@ -50,25 +75,26 @@ struct node * SortedMerge(struct node *a, struct node *b)
 /* Drier program to test above functions*/
 int main()
 {
-    struct node* res = NULL;
-    struct node* a = NULL;
-    struct node* b = NULL;
+   struct node* res = NULL;
+   struct node* a = NULL;
+   struct node* b = NULL;
 
-    /* Let us create two sorted linked lists to test
-      the functions
-       Created lists, a: 5->10->15,  b: 2->3->20 */
-    push(&a, 15);
-    push(&a, 10);
-    push(&a, 5);
+   /* Let us create two sorted linked lists to test
+     the functions
+      Created lists, a: 5->10->15,  b: 2->3->20 */
+   push(&a, 15);
+   push(&a, 10);
+   push(&a, 5);
 
-    push(&b, 20);
-    push(&b, 3);
-    push(&b, 2);
+   push(&b, 20);
+   push(&b, 3);
+   push(&b, 2);
 
-    res = SortedMerge(a, b);
+   //res = SortedMerge(a, b);
+   res = mergeList(a, b);
 
-    printf("Merged Linked List is: \n");
-    printList(res);
+   printf("Merged Linked List is: \n");
+   printList(res);
 
-    return 0;
+   return 0;
 }
