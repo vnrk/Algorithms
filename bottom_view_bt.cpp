a18c0beb8c1bf2a65a779e7b98d574d0a63254e9
vnrk <koppu.venkata@gmail.com>
2017-12-28 17:15:25 +0530

    updated

--- bottom_view_bt.cpp
+++ bottom_view_bt.cpp
@@ -23,52 +23,39 @@ void bottomView(Node *root)
     if (root == NULL)
         return;
  
-    // Initialize a variable 'hd' with 0
-    // for the root element.
+    // Initialize a variable 'hd' with 0 for the root element.
     int hd = 0;
  
-    // TreeMap which stores key value pair
-    // sorted on key value
+    // TreeMap which stores key value pair sorted on key value
     map<int, int> m;
  
-    // Queue to store tree nodes in level
-    // order traversal
+    // Queue to store tree nodes in level order traversal
     queue<Node *> q;
  
-    // Assign initialized horizontal distance
-    // value to root node and add it to the queue.
+    // Assign initialized horizontal distance value to root node and add it to the queue.
     root->hd = hd;
     q.push(root);
  
-    // Loop until the queue is empty (standard
-    // level order loop)
+    // Loop until the queue is empty (standard level order loop)
     while (!q.empty())
     {
         Node *temp = q.front();
         q.pop();
  
-        // Extract the horizontal distance value
-        // from the dequeued tree node.
+        // Extract the horizontal distance value from the dequeued tree node.
         hd = temp->hd;
  
-        // Put the dequeued tree node to TreeMap
-        // having key as horizontal distance. Every
-        // time we find a node having same horizontal
-        // distance we need to replace the data in
-        // the map.
+        // Put the dequeued tree node to TreeMap having key as horizontal distance. Every time we find a node having same horizontal distance we need to replace the data in the map.
         m[hd] = temp->data;
  
-        // If the dequeued node has a left child, add
-        // it to the queue with a horizontal distance hd-1.
+        // If the dequeued node has a left child, add it to the queue with a horizontal distance hd-1.
         if (temp->left != NULL)
         {
             temp->left->hd = hd-1;
             q.push(temp->left);
         }
  
-        // If the dequeued node has a right child, add
-        // it to the queue with a horizontal distance
-        // hd+1.
+        // If the dequeued node has a right child, add it to the queue with a horizontal distance hd+1.
         if (temp->right != NULL)
         {
             temp->right->hd = hd+1;
